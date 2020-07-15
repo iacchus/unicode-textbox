60daec41bf76d653aef297296e0527bfe7b57989
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-15 16:37:56 -0300

    ok

--- box.py
+++ box.py
@@ -2,64 +2,66 @@
 
 import sys, os
 
-top_left_char = horizontal_char = top_right_char = vertical_char =\
-        bottom_left_char = bottom_right_char = '+'
+#top_left_char = horizontal_char = top_right_char = vertical_char =\
+#        bottom_left_char = bottom_right_char = '+'
+
+# https://en.wikipedia.org/wiki/Box-drawing_character
+CHARS = { 'hz': '\u2500',
+          've': '\u2502',
+          'tl': '\u250c',
+          'tr': '\u2510',
+          'bl': '\u2514',
+          'br': '\u2518' }
 
 NEWLINE = '\n'
 
-def last_line_size(text_str):
-    return len(text_str.splitlines()[-1].expandtabs())
+PAD_SIZE = 1
+# def last_line_size(text_str):
+#     return len(text_str.splitlines()[-1].expandtabs())
 
 linelist = list(sys.stdin)
+LINES = [line.expandtabs().strip('\n') for line in linelist]
 
-#def textbox(inner_size, text):
+TEXT_WIDTH = max(map(len, LINES))
+#no_of_lines = len(lines)
+
+PAD_CHAR = '\u0020' * PAD_SIZE  # space char
+
+PROCESS_LINES_ARGS = (TEXT_WIDTH, PAD_SIZE, PAD_CHAR, CHARS['ve'])
+
+#text = str()
+#output = list()
+HZ_LINE_WIDTH = TEXT_WIDTH + (PAD_SIZE * 2)
+TOP_LINE = (CHARS['tl'] + CHARS['hz']*HZ_LINE_WIDTH + CHARS['tr'])
+
+BOTTOM_LINE = (CHARS['bl'] + CHARS['hz']*HZ_LINE_WIDTH + CHARS['br'])
 
 
-# gets the biggest line
-biggest_line_size = 0
-for line in linelist:
-    line_lenght = len(line.expandtabs().strip(NEWLINE))
-    if line_lenght > biggest_line_size:
-        biggest_line_size = line_lenght
+def process_line(line, text_width, pad_size, pad_char, ve_char):
 
-no_of_lines = len(linelist)
+    padding = pad_char * pad_size
+    extra_padding = pad_char * (text_width - len(line))
 
-padsize = 1
-padding = ' ' * padsize # space char
+    return ve_char + padding + line + extra_padding + padding + ve_char
 
-text = str()
-
-box_cols = 1 + padsize + biggest_line_size + padsize + 1
-
-for line_num, line in enumerate(linelist):
+text_list = [process_line(line, *PROCESS_LINES_ARGS) for line in LINES]
+TEXT = str('\n').join(text_list)
+box_parts = [TOP_LINE, TEXT, BOTTOM_LINE]
+box = str('\n').join(box_parts)
+#for line_num, line in enumerate(LINES):
 
     # first box line
-    if line_num ==  0:
-        text += top_left_char
-        text += horizontal_char * (1+biggest_line_size+1)  # padding
-        text += top_right_char
-        text += NEWLINE
+    #if line_num ==  0:
+    #    output.append(TOP_LINE)
 
-    if line.strip(' ') != '':
-        text += vertical_char
-        text += padding
-        #text += line.strip(' ')
-        text += line.strip(NEWLINE).expandtabs()
-        text += padding
-        extra_pad = ' ' *  (biggest_line_size - last_line_size(text))
-        #print(last_line_size(text))
-        text += extra_pad
-        text += vertical_char
-        text += NEWLINE
+    #text = process_line(line, *PROCESS_LINES_ARGS)
+    #output.append(text)
 
-    if line_num +1 == no_of_lines:
-        text += bottom_left_char
-        text += horizontal_char * (1+biggest_line_size+1)  # padding
-        text += bottom_right_char
-        text += NEWLINE
+    #if line_num + 1 == no_of_lines:
+    #    output.append(BOTTOM_LINE)
 
-#divider = spacing + ('─' * int(biggest_line_size)) # unicode 0x2500
-#text += divider
 
-print(text, end="\n"*2)
+#text = "\n".join(output)
+# print(text, end="\n"*2)
+print(box, end="\n"*2)
 
